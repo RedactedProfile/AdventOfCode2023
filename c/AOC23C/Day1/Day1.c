df620ba1ab3e35fee35b5aed73669faa6ca3d3fe
Kyle Harrison <redactedprofile@gmail.com>
2023-12-01 21:54:42 -0800

    trying to do some kind of Str=>Int map thing but it's becoming a bit of a disaster

--- c/AOC23C/Day1/Day1.c
+++ c/AOC23C/Day1/Day1.c
@@ -6,6 +6,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct StrIntMap {
+    char key[20];
+    int value;
+};
+
+static char** StrIntMapGetKeys(struct StrIntMap map[], int size)
+{
+    int i;
+    char** strArr = (char**)malloc(size * sizeof(char*));
+
+    if (strArr == NULL) {
+        printf("Memory allocation of strings failed");
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i < size; i++) {
+        strArr[i] = _strdup(map[0].key);
+    }
+
+    return strArr;
+}
+
+static int StrIntMapFindByKey(struct StrIntMap map[], int size, const char* key) 
+{
+    int i;
+
+    for (i = 0; i < size; i++) {
+        if (strcmp(map[i].key, key) == 0) {
+            return map[i].value;
+        }
+    }
+
+    return -1;
+}
 
 int main()
 {
@@ -14,6 +48,22 @@ int main()
     FILE* ptr;
     char ch,
          digits[2];
+
+    struct StrIntMap map[] = {
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 }
+    };
+    int mapSize = sizeof(map) / sizeof(map[0]);
+
+    auto value = StrIntMapFindByKey(map, mapSize, "two");
+    auto value2 = StrIntMapGetKeys(map, mapSize);
 
     printf("=====================================\n==== AOC '23, C edition \n====     by Kyle Harrison\n===================\n==== Day 1 \n=================================\n\n");
 
