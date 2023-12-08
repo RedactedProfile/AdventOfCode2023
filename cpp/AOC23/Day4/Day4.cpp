86e020c503b6b31aeac54e937d8242c3fcfedf32
Kyle Harrison <redactedprofile@gmail.com>
2023-12-07 22:15:29 -0800

    Day 4 Part 2 C++ complete

--- cpp/AOC23/Day4/Day4.cpp
+++ cpp/AOC23/Day4/Day4.cpp
@@ -10,7 +10,7 @@
 #include <ExecutionTime.h>
 
 
-#define USE_SAMPLE
+#define USE_SAMPLE2
 
 
 #ifdef USE_SAMPLE
@@ -174,7 +174,7 @@ void build_cache()
 int main()
 {
 	ExecutionTimer<std::chrono::milliseconds> timer;
-	int score, i, c;
+	int score, i, c, m;
 
 	std::cout << "Advent of Code 2023, Day 4: Scratchcards." << std::endl;
 	std::cout << "========================================" << std::endl;
@@ -191,20 +191,38 @@ int main()
 		std::cout << "Part 1 Score: " << score << std::endl;
 	}
 
-	// Part 2, find out how many winning numbers there were per card, clone that many cards below into the games cache, and re-sum up the score
+	// Part 2, find out how many winning numbers there were per card, clone that many cards into their own iterable container separated by card ID
 	{
 		score = 0;
-		std::vector<Game> games_clone(games);
-		for (i = 0; i < games_clone.size(); ++i) {
-			for (c = 0; c < games[i].total_matches(); ++c) {
-				// if there are some, clone the range
-				games.push_back(games[i + c]);
-			}
-		}
+		std::vector<std::vector<Game>> buckets = std::vector<std::vector<Game>>(games.size()); // pre-allocated a bucket-per-game 
+		// populate the buckets first element with each game
 		for (i = 0; i < games.size(); ++i) {
-			score += games[i].score();
+			Game game = games[i];
+			int id0 = game.id - 1;
+			buckets[id0].push_back(game); 
 		}
 
-		std::cout << "Part 1 Score: " << score << std::endl;
+		// with the buckets pre-allocated with a copy of each original game 
+		// now we iterate over the buckets themselves, keeping in mind a pointer to the actual game in the original games manifest. 
+		// This is important because we'll need to clone n amount of games into their respective buckets based on total matches of the game 
+		for (i = 0; i < buckets.size(); ++i) {
+			for (c = 0; c < buckets[i].size(); ++c) {
+				Game game = buckets[i][c];
+				int matches = game.total_matches();
+
+				for (m = 0; m < matches; ++m) {
+					// actually we can just clone the first element of that respective bucket
+					int id1 = i + m + 1;
+					buckets[id1].push_back(buckets[id1][0]);
+				}
+			}
+		}
+
+		// run through one more time to count the number of cards
+		for (i = 0; i < buckets.size(); ++i) {
+			score += buckets[i].size();
+		}
+
+		std::cout << "Part 2 Score: " << score << std::endl;
 	}
 }
